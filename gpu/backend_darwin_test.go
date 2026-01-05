d5592eec6b567ed233230aa878dac7825a775389
Andrey Kolkov <a.kolkov@gmail.com>
2026-01-05 15:22:38 +0300

    fix(ci): skip Metal tests on CI (virtualized macOS) (#36)
    Metal is not available on GitHub Actions macOS runners due to
    Apple Virtualization Framework limitations. Add TestMain with
    CI detection to skip these tests automatically.
    
    Reference: https://github.com/actions/runner-images/discussions/6138

--- gpu/backend_darwin_test.go
+++ gpu/backend_darwin_test.go
@@ -4,6 +4,7 @@ package gpu_test
 
 import (
 	"errors"
+	"os"
 	"testing"
 	"time"
 
@@ -12,6 +13,16 @@ import (
 	"github.com/gogpu/gogpu/gpu/types"
 	"github.com/gogpu/gogpu/internal/platform/darwin"
 )
+
+func TestMain(m *testing.M) {
+	// Skip on CI - Metal is not available on GitHub Actions macOS runners
+	// due to Apple Virtualization Framework limitations.
+	// See: https://github.com/actions/runner-images/discussions/6138
+	if os.Getenv("CI") == "true" || os.Getenv("GITHUB_ACTIONS") == "true" {
+		os.Exit(0)
+	}
+	os.Exit(m.Run())
+}
 
 const backendTestWGSL = `
 @vertex
